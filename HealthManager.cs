55c664703c88c97c5736eb5b3973357ad143dd67
GeorgeK. <156440859+GeoK2002@users.noreply.github.com>
2025-07-30 13:05:52 +0300

    Create HealthManager.cs
    Here are included 2 simple functions for the player taking damage and healing that are very important for other scripts

---
+++ HealthManager.cs
@@ -0,0 +1,39 @@
+/*Here are included 2 simple functions for the player taking damage and healing that are very important for other scripts*/
+
+public class HealthManager : MonoBehaviour
+{
+
+    public Image healthBar;
+    public float healthAmount = 100f;
+    public PlayerMovement playerMovement;
+    private SpriteRenderer spriteRenderer;
+    public DeathMenuManager deathMenuManager;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if(healthAmount <= 0)
+        {
+            deathMenuManager.ShowDeathMenu();
+            gameObject.SetActive(false);
+        }
+    }
+
+    public void TakeDamage(float damage)
+    {
+        healthAmount -= damage;
+        healthBar.fillAmount = healthAmount / 100f;
+    }
+
+    public void Heal(float healingAmount)
+    {
+        healthAmount += healingAmount;
+        healthAmount = Mathf.Clamp(healthAmount, 0, 100);
+        healthBar.fillAmount = healthAmount / 100f;
+    }
+
+}
